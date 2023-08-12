f6e24eac29a655d92ece23c07f5e83097273bfba
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-08-11 20:42:44 -0700

    adding react

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{html,js, jsx}'",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Garamond", "sans-serif"],
+    },
+    colors: {
+      "page-black": "#000000",
+      "page-white": "#FFFFFF",
+      "page-color": "#",
+      "text-color": "#008080",
+      "text-white": "#FFFFFF",
+      "text-neon": "",
+      "button-color": "",
+      "component-color": "",
+    },
+    extend: {},
+  },
+  plugins: [],
+};

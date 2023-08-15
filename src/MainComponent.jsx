5e10ef50e4b5fcc1e97e094a82a98a71c19ad6ac
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-08-15 00:13:39 -0700

    adjusting footer

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -5,7 +5,7 @@ import Focus from './components/focus/Focus';
 
 export default function MainComponent() {
     return (
-      <main className="w-full relative top-6 block p-4 py-16 gap-4 flex flex-col justify-between text-text-color">
+      <main className="w-full relative top-0 block p-4 py-16 gap-4 flex flex-col justify-between text-text-color">
         <Playlist />
         <Focus />
         <AudioBooks />

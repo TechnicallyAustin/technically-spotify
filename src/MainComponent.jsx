1063e5dc121784aebeea18bff0506bb303bd527e
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-08-14 22:54:02 -0700

    removed witdths of sectiobs

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -6,7 +6,7 @@ import AudioBookCard from './AudioBookCard';
 export default function MainComponent() {
     return (
       <main className="w-full relative top-6 block p-4 py-16 gap-12 flex flex-col justify-between text-text-color">
-        <section id="playlists" className="w-full flex flex-col py-2">
+        <section id="playlists" className="w-full flex flex-col justify-start items-start py-2">
           <header>
             <h2>Technically Playlists</h2>
           </header>
@@ -15,7 +15,7 @@ export default function MainComponent() {
 
           <section
             id="playlistContainer"
-            className=" w-screen  flex justify-start items-center gap-3  overflow-x-auto no-scrollbar"
+            className="   flex justify-start items-center gap-3  overflow-x-scroll no-scrollbar"
           >
             <MusicCard />
             <MusicCard />
@@ -31,7 +31,7 @@ export default function MainComponent() {
 
           <section
             id="focusContainer"
-            className=" w-screen flex justify-start items-center gap-3  overflow-x-auto no-scrollbar"
+            className="flex justify-start items-center gap-3  overflow-x-scroll no-scrollbar"
           >
             <FocusCard />
             <FocusCard />
@@ -45,7 +45,7 @@ export default function MainComponent() {
             <h2>Technically AudioBooks</h2>
           </header>
 
-          <section id="audiobookContainer" className="w-screen  flex gap-3  justify-start items-center overflow-x-auto no-scrollbar">
+          <section id="audiobookContainer" className="flex gap-3  justify-start items-center overflow-x-auto no-scrollbar">
             <AudioBookCard />
             <AudioBookCard />
             <AudioBookCard />

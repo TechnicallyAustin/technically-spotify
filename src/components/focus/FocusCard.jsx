e24ec03749b2b1c2b0f2ed1f895a5271e4b148db
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-08-15 01:05:50 -0700

    adjusted card heights

--- src/components/focus/FocusCard.jsx
+++ src/components/focus/FocusCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function FocusCard() {
     return (
-      <article id="focusCard" className="h-full aspect-square w-2/5 flex-none rounded-lg">
+      <article id="focusCard" className="aspect-square w-2/5 flex-none rounded-lg">
         <figure className="w-full flex flex-col justify-between items-center rounded gap-2">
           <img src="" alt="" id="" className="w-full h-full aspect-square " />
           <figcaption className="w-full flex justify-start items-end">

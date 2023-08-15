a46c5bd5b9564e14cf3020571e10c3a32d227df5
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-08-15 01:10:11 -0700

    adjusted main gap

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -5,7 +5,7 @@ import Focus from './components/focus/Focus';
 
 export default function MainComponent() {
     return (
-      <main className="w-full h-screen relative top-0 block p-4 py-12 gap-4  flex flex-col justify-center text-text-color">
+      <main className="w-full h-screen relative top-0 block p-4 py-12 gap-2  flex flex-col justify-center text-text-color">
         <Playlist />
         <Focus />
         <AudioBooks />

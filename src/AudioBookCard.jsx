6060ab20e6bb5f8475dc2136ddbdf7d961742503
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-08-14 22:49:46 -0700

    remove scaffolding

--- src/AudioBookCard.jsx
+++ src/AudioBookCard.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function AudioBookCard() {
     return (
       <article id="audioBookCard" className="aspect-square w-2/5 flex-none rounded-lg">
-        <figure className="w-full h-full flex flex-col justify-between items-center rounded gap-2  ">
+        <figure className="w-full flex flex-col justify-between items-center rounded gap-2  ">
           <img src="" alt="" id="" className="w-full h-full aspect-square " />
           <figcaption className="w-full flex flex-col justify-center items-start">
             <p className="text-xs">Fig Caption</p>
--- src/MusicCard.jsx
+++ src/MusicCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MusicCard() {
     return (
-      <article id="musicCard" className="h-full w-2/5 aspect-square flex-none rounded-lg">
+      <article id="musicCard" className="w-2/5 aspect-square flex-none rounded-lg">
         <figure className="w-full h-full flex flex-col justify-between items-center rounded gap-2 ">
           <img src="" alt="" id="" className="w-full h-full aspect-square " />
           <figcaption className="w-full flex justify-start items-end">

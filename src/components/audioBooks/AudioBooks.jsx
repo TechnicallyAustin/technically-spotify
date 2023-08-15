fa17743e2a4ebf13781d57dd9d7b728c76e8328e
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-08-15 00:20:32 -0700

    adjusting container sizes and main size

--- src/components/audioBooks/AudioBooks.jsx
+++ src/components/audioBooks/AudioBooks.jsx
@@ -3,7 +3,7 @@ import AudioBookCard from './AudioBookCard';
 
 export default function AudioBooks() {
     return (
-      <section id="audioBooks" className="w-full  flex flex-col py-2 gap-2">
+      <section id="audioBooks" className="w-full h-1/3  flex flex-col py-2 gap-2">
         <header className='text-page-white'>
           <h2>Technically AudioBooks</h2>
         </header>

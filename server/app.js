d717f4c795ab76136770aad662b3718da9a0c319
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-09-14 17:19:24 -0700

    preparing to implement passport-spotify

--- server/app.js
+++ server/app.js
@@ -1,28 +1,80 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
-
+const session = require("express-session");
 const mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const User = require("./models/user");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(session({secret: "session-secret", resave: false, saveUninitialized: true}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 // SPOTIFY API
+  const clientID = process.env.CLIENT_ID
+  const clientSecret = process.env.CLIENT_SECRET
+  const responseType = process.env.RESPONSE_TYPE
+  const redirectURI = process.env.REDIRECT_URI
+  const query = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${redirectURI}&scope=user-top-read`
+
+
+// PASSPORT SPOTIFY Strategy
+const SpotifyStrategy = require("passport-spotify").Strategy;
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: clientID,
+      clientSecret: clientSecret,
+      callbackURL: "http://localhost:8888/auth/spotify/callback",
+    },
+    function (accessToken, refreshToken, expires_in, profile, done) {
+      User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+    }
+  )
+);
+
 
 // PASSPORT LOCAL STRATEGY
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy
-const authObject = {}
 
-  // PASSPORT LOCAL INITIALIZATIONS
-     // app.use(passport.session())
-    // app.use(passport.initialize())
+const authUser = async (username, password, done) => {
+  try {
+    const user = await getUsersCollection().findOne({ username: username });
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      return done(null, user);
+    }
+    if (!user) {
+      done(null, false);
+    }
+    if (!match) {
+      done(null, false);
+    }
+  } catch (err) {
+    return done(err);
+  }
+};
 
-  // PASSPORT SESSIONS
+passport.use(new LocalStrategy(authUser));
+
+passport.serializeUser((user, done) => {
+  return done(null, user.username);
+})
+
+passport.deserializeUser(async function (user, done) {
+  return done(null, user);
+});
+
 
 // PASSPORT JWT STRATEGY
 
@@ -53,6 +105,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES and ROUTE PROTECTION
+  // app.use("/", authRouter);
+  // function ensureAuthenticated(req, res, next) {
+  //   console.log("Authenticated", req.isAuthenticated(), req.user);
+  //   if (req.isAuthenticated()) {
+  //     return next(); // continue to the route
+  //   }
+  //   if (!req.isAuthenticated()) {
+  //     res.redirect("/login"); // Redirect to login if not authenticated
+  //   }
+  // }
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- server/models/user.js
+++ server/models/user.js
@@ -3,14 +3,35 @@ const Schema = mongoose.Schema
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    first_name: {},
-    given_name: {},
-    username: {},
-    password: {},
-    playlists: {},
-    audiobooks: {},
-    liked_songs: {}
-})
+  first_name: {
+    type: String,
+    required: true,
+  },
+  given_name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  playlists: {
+    type: Array,
+    required: true,
+  },
+  audiobooks: {
+    type: Array,
+    required: true,
+  },
+  liked_songs: {
+    type: Array,
+    required: true,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 
